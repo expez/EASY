45357f7afa69649a54b635642bb4289d541023aa
Expez <mrgrandmother@gmail.com>
2012-01-27 11:11:00 +0100

    Removed reproductionmechanism interface.

--- src/edu/ntnu/EASY/reproductionMechanism/ReproductionMechanism.java
+++
@@ -1,7 +0,0 @@
-package edu.ntnu.EASY.reproductionMechanism;
-
-import edu.ntnu.EASY.individual.Individual;
-public interface ReproductionMechanism {
-    
-    public Individual getChild( Individual mom, Individual dad);
-}
