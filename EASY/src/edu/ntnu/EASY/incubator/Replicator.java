e84cfb5835e5bb7e47cac9687e7add02be488cb1
Tormund Haus <tormundhaus@gmail.com>
2012-02-14 14:14:16 +0100

    Removed setEnvironment

--- EASY/src/edu/ntnu/EASY/incubator/Replicator.java
+++ EASY/src/edu/ntnu/EASY/incubator/Replicator.java
@@ -1,11 +1,7 @@
 package edu.ntnu.EASY.incubator;
 
-import edu.ntnu.EASY.Environment;
-
 public interface Replicator<GType> {
 
-	public void setEnvironment(Environment env);
-	
 	public GType mutate(GType genome);
 	
 	public GType combine(GType g1, GType g2);
