41078c430d37e3d5e7b1c47c08d1d4db69f3f9ac
Tormund Haus <tormundhaus@gmail.com>
2012-02-14 14:32:44 +0100

    Added more output to report.

--- EASY/src/edu/ntnu/EASY/Main.java
+++ EASY/src/edu/ntnu/EASY/Main.java
@@ -16,42 +16,31 @@ You should have received a copy of the GNU General Public License
     along with EASY.  If not, see <http://www.gnu.org/licenses/>.*/
 package edu.ntnu.EASY;
 
-import edu.ntnu.EASY.blotto.BlottoFitnessCalculator;
-import edu.ntnu.EASY.blotto.BlottoIncubator;
-import edu.ntnu.EASY.blotto.BlottoReplicator;
-import edu.ntnu.EASY.incubator.BitvectorIncubator;
-import edu.ntnu.EASY.incubator.BitvectorReplicator;
-import edu.ntnu.EASY.incubator.Incubator;
-import edu.ntnu.EASY.individual.BitvectorIndividual;
-import edu.ntnu.EASY.individual.Individual;
-import edu.ntnu.EASY.selection.adult.AdultSelector;
-import edu.ntnu.EASY.selection.adult.FullGenerationalReplacement;
-import edu.ntnu.EASY.selection.parent.FitnessProportionateSelector;
-import edu.ntnu.EASY.selection.parent.ParentSelector;
-d
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 import edu.ntnu.EASY.blotto.Blotto;
 
 public class Main {
     
     public static void main(String[] args) {
-//    	int[] Bs = {5,10,20};
-//    	double[] Rfs = {1.0,0.5,0.0};
-//    	double[] Lfs = {1.0,0.5,0.0};
-
-    	int[] Bs = {5,20};
-    	double[] Rfs = {1.0,0.0};
-    	double[] Lfs = {1.0,0.0};
-    	
+    	int[] Bs = {5,10,20};
+    	double[] Rfs = {1.0,0.5,0.0};
+    	double[] Lfs = {1.0,0.5,0.0};
     	
     	Blotto blotto = new Blotto();
     	
     	for(int B : Bs){
     		for(double Rf : Rfs){
     			for(double Lf : Lfs){
+    				double start = System.currentTimeMillis();
     				Report<double[],double[]> report = blotto.runBlottoEvolution(B,Rf,Lf);
-    				String filename = String.format("blotto-%d-%.2f-%.2f.txt",B,Rf,Lf);
+    				double stop = System.currentTimeMillis();
+    				String filename = String.format("log/blotto-%d-%.2f-%.2f.txt",B,Rf,Lf);
     				try {
 						PrintStream out = new PrintStream(new FileOutputStream(filename));
+						out.printf("# Blotto run, B: %d, Rf: %.2f, Lf: %.2f - %.2f sec %n",B,Rf,Lf,(stop-start)/1000);
 						report.writeToStream(out);
 					} catch (FileNotFoundException e) {
 						System.err.printf("File not found: %s%n",filename);
