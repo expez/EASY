ff8ab79ea3ff0507ab9eb9aa3ff1be8314fea4af
Expez <mrgrandmother@gmail.com>
2012-02-25 22:04:27 +0100

    Add fitness measure using Waveform distance metric.

---
+++ EASY/src/edu/ntnu/EASY/neuronModel/NeuronFitnessCalculatorWDM.java
@@ -0,0 +1,29 @@
+package edu.ntnu.EASY.neuronModel;
+
+/**
+ * Fitness calculator using the waveform distance metric to calculate fitness. 
+ * The distance measure used is the sum of the differences in voltages for all
+ * points in the spike train.
+ *
+ */
+public class NeuronFitnessCalculatorWDM extends AbstractNeuronFitnessCalculator {
+
+	public NeuronFitnessCalculatorWDM(double[] target) {
+		super(target);
+	}
+
+	@Override
+	public double calculate(double[] phenome) {
+		double delta = 0;
+		//Sum for 1 to lenght - 1, first entry is seed value and irrelevant.
+		for (int i = 1; i < phenome.length - 1; i++) {
+			//Absolute distance is what matters.
+			delta += Math.pow(phenome[i] - target[i], 2);
+		}
+		//Normalize
+		delta = Math.sqrt(delta);
+		// Turn into fitness value in interval [0,1].
+		return 1 / (1 + delta);
+	}
+
+}
