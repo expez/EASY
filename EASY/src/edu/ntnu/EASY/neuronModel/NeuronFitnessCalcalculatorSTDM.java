b17cf339ccbff7fac78a2b32d15099cf59bbe0d8
Tormund Haus <tormundhaus@gmail.com>
2012-02-28 11:21:17 +0100

    Refactoring FitnessCalculators

--- EASY/src/edu/ntnu/EASY/neuronModel/NeuronFitnessCalcalculatorSTDM.java
+++ EASY/src/edu/ntnu/EASY/neuronModel/NeuronFitnessCalcalculatorSTDM.java
@@ -17,33 +17,20 @@ public class NeuronFitnessCalcalculatorSTDM extends AbstractNeuronFitnessCalcula
 	@Override
 	public double calculate(double[] phenome) {
 		List<Integer> spikeTimes = getSpikeTimes(phenome);
-		List<Integer> targetSpikeTimes = getSpikeTimes(target);
-		return 1 / ( 1 + calculateDistance(spikeTimes, targetSpikeTimes ) - calculatePenalty(spikeTimes.size(), targetSpikeTimes.size(), phenome.length));		
+		double delta = calculateDistance(spikeTimes, targetSpikeTimes);
+		delta -= calculatePenalty(spikeTimes.size(), phenome.length); 
+		return 1 / (1 + delta);		
 	}
 	
-	private double calculateDistance(List<Integer> spikeList, List<Integer> targetSpikeList) {
+	private double calculateDistance(List<Integer> spikeList, int[] targetSpikeTimes) {
 		
 		//We have to find the shortest list to iterate over.
-		int shortestListLength= Math.min(spikeList.size(),targetSpikeList.size());
+		int shortestListLength= Math.min(spikeList.size(),targetSpikeTimes.length);
 		
 		double sum = 0;
 		for (int spikeTime = 0; spikeTime < shortestListLength; spikeTime++) {
-			sum += Math.pow( spikeList.get(spikeTime) + targetSpikeList.get(spikeTime), 2);
+			sum += Math.pow( spikeList.get(spikeTime) + targetSpikeTimes[spikeTime], 2);
 		}
-		sum = Math.sqrt(sum);
-		sum /= shortestListLength;
-		return sum;
+		return Math.sqrt(sum) / shortestListLength;
 	}
-
-	//Calculate a penalty proportional to the difference in number of spikes.
-	private double calculatePenalty(int spikes1, int spikes2, int L) {
-		int N = Math.max(spikes1,spikes2);
-		double M = Math.min(spikes1,spikes2);
-		if( M == 0) {
-			M = 0.001;
-		}
-		return ((N - M) * L )/ (2.0 * M);
-	}
-	
-	
 }
--- EASY/src/edu/ntnu/EASY/neuronModel/NeuronFitnessCalculatorSIDM.java
+++ EASY/src/edu/ntnu/EASY/neuronModel/NeuronFitnessCalculatorSIDM.java
@@ -2,8 +2,6 @@ package edu.ntnu.EASY.neuronModel;
 
 import java.util.List;
 
-import edu.ntnu.EASY.Population;
-
 /**
  * Fitness calculator using the Spike Interval Distance Metric to calculate fitness. 
  * A spike interval is simply the gaps between successive spike times.
@@ -15,26 +13,20 @@ public class NeuronFitnessCalculatorSIDM extends AbstractNeuronFitnessCalculator
 	}
 
 	@Override
-	public void setPopulation(Population<?, double[]> population) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public double calculate(double[] phenome) {
 		List<Integer> spikeTimes = getSpikeTimes(phenome); 
-		List<Integer> targetSpikeTimes = getSpikeTimes(target);
-		int length = Math.min(spikeTimes.size(), targetSpikeTimes.size());
+		int length = Math.min(spikeTimes.size(), targetSpikeTimes.length);
 		int spikeTimeDelta;
 		int targetSpikeTimeDelta;
 		double distance = 0;
 		for (int i = 1; i < length - 1; i++) {
 			//Get difference in spiketimes for the two spike trains.
 			spikeTimeDelta = spikeTimes.get(i) - spikeTimes.get(i - 1);
-			targetSpikeTimeDelta = targetSpikeTimes.get(i) - targetSpikeTimes.get(i - 1);
+			targetSpikeTimeDelta = targetSpikeTimes[i] - targetSpikeTimes[i - 1];
 			//Square and sum absolute distance.
 			distance += Math.pow( spikeTimeDelta - targetSpikeTimeDelta, 2);
 		}
+		distance -= calculatePenalty(spikeTimes.size(),phenome.length);		
 		//Sqrt to normalize.
 		distance = Math.sqrt(distance) / (length - 1);
 		//Turn into fitness value in interval [0,1]
